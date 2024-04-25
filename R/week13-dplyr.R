b5a36a7ae1e8d7ab53d4770982360932f7e36c5a
Toni Merhar <merha013@umn.edu>
2024-04-25 16:54:12 -0500

    Project Complete.

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -15,43 +15,63 @@ conn <- dbConnect(MariaDB(),
 show_databases <- dbGetQuery(conn,"SHOW DATABASES;") # show available databases
 dbExecute(conn, "USE cla_tntlab;")
 
-## download all three datasets on the UMN SQL server
-employees_tbl <- dbGetQuery(conn, "SELECT * FROM 
-                            cla_tntlab.datascience_employees;")
-testscores_tbl <- dbGetQuery(conn, "SELECT * FROM 
-                             cla_tntlab.datascience_testscores;")
-offices_tbl <- dbGetQuery(conn, "SELECT * FROM 
-                          cla_tntlab.datascience_offices;")
+## Download three dataset on UMN SQL server
+employees_tbl <- dbGetQuery(  
+  conn, "SELECT * FROM cla_tntlab.datascience_employees;")
+testscores_tbl <- dbGetQuery(  
+  conn, "SELECT * FROM cla_tntlab.datascience_testscores;")
+offices_tbl <- dbGetQuery(  
+  conn, "SELECT * FROM cla_tntlab.datascience_offices;")
 
-## Save them as .csv files 
-write_csv(employees_tbl, "../data/employees.csv")
-write_csv(testscores_tbl, "../data/testscores.csv")
-write_csv(offices_tbl, "../data/offices.csv")
+## Save as .csv file in data folder
+write_csv(employees_tbl, "../data/employees.csv") 
+write_csv(testscores_tbl, "../data/testscores.csv")  
+write_csv(offices_tbl, "../data/offices.csv")  
 
-dbDisconnect(conn) # warning msg advised to use this when finished w/connection
+dbDisconnect(conn)  # warning msg advised to use when finished w/connection
 
-## Combine the data with joins only
-week13_tbl <- employees_tbl %>%
+## Combine data with joins only
+week13_tbl <- employees_tbl %>%  
   inner_join(testscores_tbl, by = join_by(employee_id)) %>% 
     ## inner_join only keeps observations from x that have a matching key in y
+    ## which means it removes employees without test scores
   full_join(offices_tbl, by = join_by("city" == "office"))
 
-## Save this file as week13.csv and place in out folder
+## Save as .csv file in out folder
 write_csv(week13_tbl, "../out/week13.csv")
 
 
 # Analysis
-nrow(week13_tbl) # total number of managers
-length(unique(week13_tbl$employee_id)) # total number of unique managers by ID
-  ## unique returns a vector with duplicates removed, but there were none
 
-## summary of number of managers
-week13_tbl %>%
+## Total number of managers
+summary1 <- nrow(week13_tbl)  
+print(summary1)
+
+## Total number of unique managers by ID
+summary2 <- length(unique(week13_tbl$employee_id))  
+  ## unique() returns a vector w/duplicates removed, but there were none
+print(summary2)
+
+## Display summary of number of managers
+summary3 <- week13_tbl %>%  
   filter(manager_hire == "N") %>% 
     ## include only those who were not originally hired as managers
   group_by(city) %>% # split by location (city)
-  View
+  summarise(num_managers = n())
+print(summary3)
 
-## Display the mean and standard deviation of number of years of employment split by performance level (bottom, middle, and top) - in addition to dplyr functions, you can use mean() and sd() for this task.
+## Display mean & sd of muber of years of employment
+summary4 <- week13_tbl %>%  
+  group_by(performance_group) %>% # split by performance level
+  summarise(mean_years = mean(yrs_employed),
+            sd_years = sd(yrs_employed))
+print(summary4)
 
-## Display each manager's location classification (urban vs. suburban), ID number, and test score, in alphabetical order by location type and then descending order of test score.
+## Display manager location type, ID, & test score
+summary5 <- week13_tbl %>%  
+  select(employee_id, type, test_score) %>%
+  arrange(type, desc(test_score))
+    ## arranged in alphabetical order by location type 
+    ## then descending order of test score
+print(summary5) # it would be easier to look at this dataset with View() 
+View(summary5)
--- R/week13-sql.R
+++ R/week13-sql.R
@@ -1,31 +1,78 @@
 # Script Settings and Resources
+library(tidyverse)
+library(RMariaDB)
+library(keyring)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
-## Download the three datasets you have access to on the UMN SQL server.
-
-# Save them as employees_tbl, testscores_tbl, and offices_tbl
-# Save them as employees.csv, testscores.csv., and offices.csv 
-
-# Create a dataset called week13_tbl combining the data such that employees without test scores are removed. 
-# You must do this with joins only. 
-# Save this file as week13.csv (you can place this in out).
-
+conn <- dbConnect(MariaDB(),
+                  user="merha013",
+                  password=key_get("latis-mysql","merha013"),
+                  host="mysql-prod5.oit.umn.edu",
+                  port=3306,
+                  ssl.ca='../mysql_hotel_umn_20220728_interm.cer')
+dbExecute(conn, "USE cla_tntlab;")
 
 
 # Analysis
-## using SQL calls alone, using data consisting only of employees that have test scores:
 
-## Display the total number of managers.
+## Total number of managers
+results1 <- dbGetQuery(  
+  conn, "SELECT COUNT(*) AS total_managers 
+  FROM cla_tntlab.datascience_employees
+  WHERE employee_id IN (
+  SELECT DISTINCT employee_id    # selecting only those with test scores
+  FROM cla_tntlab.datascience_testscores);")
+print(results1)
 
-## Display the total number of unique managers (i.e., unique by id number).
+## Total number of unique managers by ID
+results2 <- dbGetQuery(  
+  conn, "SELECT COUNT(DISTINCT employee_id) 
+  AS total_unique_managers 
+  FROM cla_tntlab.datascience_employees
+  WHERE employee_id IN (
+    SELECT DISTINCT employee_id    # selecting only those with test scores
+    FROM cla_tntlab.datascience_testscores);")
+print(results2)
 
-## Display a summary of the number of managers split by location, but only include those who were not originally hired as managers.
-SELECT location, COUNT(*) AS num_managers
-FROM employees
-WHERE position = 'Manager'
-AND hired_as_manager = 'No'
-GROUP BY location;
+## Display summary of number of managers
+results3 <- dbGetQuery(  
+  conn, "SELECT city, COUNT(*) AS num_managers
+  FROM cla_tntlab.datascience_employees
+  WHERE employee_id IN (
+    SELECT DISTINCT employee_id    # selecting only those with test scores
+    FROM cla_tntlab.datascience_testscores)
+  AND manager_hire = 'N'     # only those not origionally hired as managers
+  GROUP BY city;")     # split by location (city)
+print(results3)
 
-## Display the mean and standard deviation of number of years of employment split by performance level (bottom, middle, and top) - in addition to dplyr functions, you can use mean() and sd() for this task.
+## Display mean & sd of number of years of employment
+results4 <- dbGetQuery(  
+  conn, "SELECT performance_group, 
+  AVG(yrs_employed) AS mean_years,
+  STDDEV(yrs_employed) AS sd_years
+  FROM cla_tntlab.datascience_employees
+  WHERE employee_id IN (
+    SELECT DISTINCT employee_id    # selecting only those with test scores
+    FROM cla_tntlab.datascience_testscores)
+  GROUP BY performance_group;")     # split by performance level
+print(results4)
 
-## Display each manager's location classification (urban vs. suburban), ID number, and test score, in alphabetical order by location type and then descending order of test score.
+## Display manager location type, ID, & test score
+results5 <- dbGetQuery(  
+  conn, "SELECT o.type, e.employee_id, t.test_score
+  FROM cla_tntlab.datascience_employees AS e
+  INNER JOIN cla_tntlab.datascience_testscores AS t
+  ON e.employee_id = t.employee_id
+  INNER JOIN cla_tntlab.datascience_offices AS o
+  ON e.city = o.office
+  WHERE e.employee_id IN (
+    SELECT DISTINCT employee_id    # selecting only those with test scores
+    FROM cla_tntlab.datascience_testscores)
+  ORDER BY o.type ASC, t.test_score DESC;") 
+    ## arranged in alphabetical order by location type 
+    ## then descending order of test score
+print(results5) # it would be easier to look at this dataset with View()
+View(results5)
+
+dbDisconnect(conn) 
